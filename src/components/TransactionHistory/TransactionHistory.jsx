6ca1a175bad026fc2b03d2e21ce71292696b2848
Matematuka <143654489+Matematuka@users.noreply.github.com>
2024-03-15 22:40:29 +0200

    Task-3 _css

--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -2,24 +2,24 @@ import items from "../../transactions.json";
 import css from "./TransactionHistory.module.css"
 
 const TransactionHistory = () => {
-    return (
-  <table className={css.table}>
-  <thead>
-    <tr>
-      <th className={css.listName}>Type</th>
-      <th className={css.listName}>Amount</th>
-      <th className={css.listName}>Currency</th>
-    </tr>
-  </thead>
-            {items.map((item) => 
-                <tbody key={item.id}>
-                    <tr className={css.listRow}>
-                        <td  className={css.listElem}>{item.type}</td>
-                        <td  className={css.listElem}>{item.amount}</td>
-                        <td  className={css.listElem}>{item.currency}</td>
-                    </tr>
-                </tbody>
-            )}
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th className={css.listName}>Type</th>
+          <th className={css.listName}>Amount</th>
+          <th className={css.listName}>Currency</th>
+        </tr>
+      </thead>
+      {items.map((item) =>
+        <tbody key={item.id}>
+          <tr className={css.listRow}>
+            <td>{item.type}</td>
+            <td>{item.amount}</td>
+            <td>{item.currency}</td>
+          </tr>
+        </tbody>
+      )}
     </table>
   )
 }
